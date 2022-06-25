66e34676a910a8af2b0f94d357fd9fc70aaaf70b
cheng <2916590935@qq.com>
2022-06-25 16:27:23 +0800

    Initial commit

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ headers = {
         'Referer': 'http://www.kuwo.cn/search/list?key=%E5%91%A8%E6%9D%B0%E4%BC%A6',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3880.400 QQBrowser/10.8.4554.400',
     }
+
 while True:
     imput_2 = input("输入人名或歌名：")
     name = parse.quote(imput_2)
